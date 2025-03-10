44c76200ed898be5c1e6a7e7bc5922cd8cdf333c
Freddie Cross <113540926+CompactPrism@users.noreply.github.com>
2025-03-10 16:26:37 +0000

    Made C++ classes for hitscan and projectile weapons

---
+++ Source/ProjectBartender/Private/Weapon_Base.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon_Base.h"
+
+
+// Sets default values
+AWeapon_Base::AWeapon_Base()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = _Mesh;
+}
+
+void AWeapon_Base::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWeapon_Base::Fire_Implementation(FVector Reticle, FVector Direction)
+{
+	
+	UE_LOG(LogTemp, Warning, TEXT("YIPPPEEEEE"));
+}
+
+void AWeapon_Base::StopFiring()
+{
+	
+}
